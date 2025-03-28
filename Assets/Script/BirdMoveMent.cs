95bce2709df8ff4beb114cf0af780610a74dd3aa
Preamps <Preampstv@gmail.com>
2025-03-28 19:21:14 +0700

    BirdMoveMent

---
+++ Assets/Script/BirdMoveMent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class BirdMoveMent : MonoBehaviour
+{
+    public float speed = 3f;
+    private Vector3 moveDirection;
+
+    void Start()
+    {
+        InvokeRepeating(nameof(ChangeDirection), 0, 3f); // เปลี่ยนทิศทางทุก 3 วิ
+    }
+
+    void Update()
+    {
+        transform.Translate(moveDirection * speed * Time.deltaTime);
+    }
+
+    void ChangeDirection()
+    {
+        float moveX = Random.Range(-1f, 1f);
+        float moveZ = Random.Range(-1f, 1f);
+        moveDirection = new Vector3(moveX, 0, moveZ).normalized; // ทำให้ทิศทางคงที่
+    }
+}
